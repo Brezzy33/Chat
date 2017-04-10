e56d1a13e97ba5c4f4956aa98e5612583bc0d58f
BreZzy33 <kenni1@live.ru>
2017-04-10 11:10:53 +0700

    Создали интерфейс 2ух форм.

--- Chat/Chat/Form1.Designer.cs
+++ Chat/Chat/Form1.Designer.cs
@@ -28,12 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sndBtn = new System.Windows.Forms.Button();
+            this.joinBtn = new System.Windows.Forms.Button();
+            this.msgTextBox = new System.Windows.Forms.RichTextBox();
+            this.chatTextBox = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fontBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sndBtn
+            // 
+            this.sndBtn.Location = new System.Drawing.Point(864, 424);
+            this.sndBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.sndBtn.Name = "sndBtn";
+            this.sndBtn.Size = new System.Drawing.Size(100, 95);
+            this.sndBtn.TabIndex = 0;
+            this.sndBtn.Text = "Отправить";
+            this.sndBtn.UseVisualStyleBackColor = true;
+            // 
+            // joinBtn
+            // 
+            this.joinBtn.Location = new System.Drawing.Point(864, 31);
+            this.joinBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.joinBtn.Name = "joinBtn";
+            this.joinBtn.Size = new System.Drawing.Size(100, 22);
+            this.joinBtn.TabIndex = 1;
+            this.joinBtn.Text = "Войти";
+            this.joinBtn.UseVisualStyleBackColor = true;
+            // 
+            // msgTextBox
+            // 
+            this.msgTextBox.Location = new System.Drawing.Point(16, 424);
+            this.msgTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.msgTextBox.Name = "msgTextBox";
+            this.msgTextBox.Size = new System.Drawing.Size(833, 95);
+            this.msgTextBox.TabIndex = 2;
+            this.msgTextBox.Text = "";
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Location = new System.Drawing.Point(16, 61);
+            this.chatTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.ReadOnly = true;
+            this.chatTextBox.Size = new System.Drawing.Size(950, 334);
+            this.chatTextBox.TabIndex = 3;
+            this.chatTextBox.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(724, 31);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(977, 27);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.менюToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(57, 21);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem.Text = "Настройки";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.infoToolStripMenuItem.Text = "Инфо";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(623, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введите ник:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 402);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите ваше сообщение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 34);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Окно чата:";
+            // 
+            // fontBtn
+            // 
+            this.fontBtn.Location = new System.Drawing.Point(864, 61);
+            this.fontBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.fontBtn.Name = "fontBtn";
+            this.fontBtn.Size = new System.Drawing.Size(100, 22);
+            this.fontBtn.TabIndex = 1;
+            this.fontBtn.Text = "Шрифт";
+            this.fontBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(977, 532);
+            this.Controls.Add(this.fontBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chatTextBox);
+            this.Controls.Add(this.msgTextBox);
+            this.Controls.Add(this.joinBtn);
+            this.Controls.Add(this.sndBtn);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Чат";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button sndBtn;
+        private System.Windows.Forms.Button joinBtn;
+        private System.Windows.Forms.RichTextBox msgTextBox;
+        private System.Windows.Forms.RichTextBox chatTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button fontBtn;
     }
 }
 
--- Chat/Chat/Form1.cs
+++ Chat/Chat/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Net;
 
 namespace Chat
 {
     public partial class Form1 : Form
     {
+        static public Socket Client;
+        private IPAddress ip = null;
         public Form1()
         {
             InitializeComponent();
+            chatTextBox.ReadOnly = true;
+            msgTextBox.Enabled = false;
+            sndBtn.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Settings stn = new Settings();
+            stn.Show();
         }
     }
 }
