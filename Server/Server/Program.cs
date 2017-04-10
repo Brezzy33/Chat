bed0a5c397529df9276b0a7ce0d03ce5e25fcaa5
BreZzy33 <kenni1@live.ru>
2017-04-10 14:54:25 +0700

    Создали серверный компонент.

---
+++ Server/Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Port;
+            //Получение имени пк.
+            string host = System.Net.Dns.GetHostName();
+            //Получения ip-адреса.
+            System.Net.IPAddress ip = System.Net.Dns.GetHostEntry(host).AddressList[0];
+            Console.WriteLine("Ip adress " + ip.ToString());
+            Console.WriteLine("Введите номер порта: ");
+            Port = Convert.ToInt32(Console.ReadLine());
+            Server server = new Server(Port, Console.Out);
+            Console.WriteLine("Starting...");
+            server.Work();
+        }
+    }
+}
